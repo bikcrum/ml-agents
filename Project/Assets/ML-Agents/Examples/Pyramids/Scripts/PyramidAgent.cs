b1e80ed84223a443aa90b8356394ac7db75434db
bikram <bikcrum@gmail.com>
2022-12-30 18:15:49 -0800

    update

--- Project/Assets/ML-Agents/Examples/Pyramids/Scripts/PyramidAgent.cs
+++ Project/Assets/ML-Agents/Examples/Pyramids/Scripts/PyramidAgent.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -6,39 +7,38 @@ using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 
-public class PyramidAgent : Agent
-{
-    public GameObject area;
+public class PyramidAgent : Agent {
+    // public GameObject area;
+
     // PyramidArea m_MyArea;
     Rigidbody m_AgentRb;
     PyramidSwitch m_SwitchLogic;
     public GameObject areaSwitch;
     public bool useVectorObs;
 
-    public override void Initialize()
-    {
+    [SerializeField] private List<Transform> obstaclesTransform;
+
+    [SerializeField] private bool randomizeObstacleRotation;
+
+    public override void Initialize() {
         m_AgentRb = GetComponent<Rigidbody>();
         // m_MyArea = area.GetComponent<PyramidArea>();
         m_SwitchLogic = areaSwitch.GetComponent<PyramidSwitch>();
     }
 
-    public override void CollectObservations(VectorSensor sensor)
-    {
-        if (useVectorObs)
-        {
+    public override void CollectObservations(VectorSensor sensor) {
+        if (useVectorObs) {
             // sensor.AddObservation(m_SwitchLogic.GetState());
             sensor.AddObservation(transform.InverseTransformDirection(m_AgentRb.velocity));
         }
     }
 
-    public void MoveAgent(ActionSegment<int> act)
-    {
+    public void MoveAgent(ActionSegment<int> act) {
         var dirToGo = Vector3.zero;
         var rotateDir = Vector3.zero;
 
         var action = act[0];
-        switch (action)
-        {
+        switch (action) {
             case 1:
                 dirToGo = transform.forward * 1f;
                 break;
@@ -52,40 +52,33 @@ public class PyramidAgent : Agent
                 rotateDir = transform.up * -1f;
                 break;
         }
+
         transform.Rotate(rotateDir, Time.deltaTime * 200f);
         m_AgentRb.AddForce(dirToGo * 2f, ForceMode.VelocityChange);
     }
 
-    public override void OnActionReceived(ActionBuffers actionBuffers)
-
-    {
+    public override void OnActionReceived(ActionBuffers actionBuffers) {
         AddReward(-1f / MaxStep);
         MoveAgent(actionBuffers.DiscreteActions);
     }
 
-    public override void Heuristic(in ActionBuffers actionsOut)
-    {
+    public override void Heuristic(in ActionBuffers actionsOut) {
         var discreteActionsOut = actionsOut.DiscreteActions;
-        if (Input.GetKey(KeyCode.D))
-        {
+        if (Input.GetKey(KeyCode.D)) {
             discreteActionsOut[0] = 3;
         }
-        else if (Input.GetKey(KeyCode.W))
-        {
+        else if (Input.GetKey(KeyCode.W)) {
             discreteActionsOut[0] = 1;
         }
-        else if (Input.GetKey(KeyCode.A))
-        {
+        else if (Input.GetKey(KeyCode.A)) {
             discreteActionsOut[0] = 4;
         }
-        else if (Input.GetKey(KeyCode.S))
-        {
+        else if (Input.GetKey(KeyCode.S)) {
             discreteActionsOut[0] = 2;
         }
     }
 
-    public override void OnEpisodeBegin()
-    {
+    public override void OnEpisodeBegin() {
         var enumerable = Enumerable.Range(0, 9).OrderBy(x => Guid.NewGuid()).Take(9);
         var items = enumerable.ToArray();
 
@@ -96,6 +89,12 @@ public class PyramidAgent : Agent
         transform.rotation = Quaternion.Euler(new Vector3(0f, Random.Range(0, 360)));
 
         m_SwitchLogic.ResetSwitch(items[1], items[2]);
+
+        if (randomizeObstacleRotation) {
+            foreach (var obstacleTransform in obstaclesTransform) {
+                obstacleTransform.Rotate(0f, Random.Range(0f, 180f), 0f);
+            }
+        }
         // m_MyArea.CreateStonePyramid(1, items[3]);
         // m_MyArea.CreateStonePyramid(1, items[4]);
         // m_MyArea.CreateStonePyramid(1, items[5]);
@@ -104,10 +103,8 @@ public class PyramidAgent : Agent
         // m_MyArea.CreateStonePyramid(1, items[8]);
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("switchOff"))
-        {
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("switchOff")) {
             SetReward(2f);
             EndEpisode();
         }
